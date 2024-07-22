50e8e4b7285ca42bb7a1d7797eeb25fb1205c7c2
Faiza Hamid <hamid.faiza811@gmail.com>
2024-07-22 13:13:38 +0300

    added seeders2

--- scripts/seedStudents.js
+++ scripts/seedStudents.js
@@ -2,15 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    console.log('Starting to seed students...');
     await queryInterface.bulkInsert('Students', [
       { first_name: 'Yulia', last_name: 'Prihodko', createdAt: new Date(), updatedAt: new Date() },
       { first_name: 'Ivan', last_name: 'Zbaraski', createdAt: new Date(), updatedAt: new Date() },
       { first_name: 'Faiza', last_name: 'Hamid', createdAt: new Date(), updatedAt: new Date() },
       { first_name: 'Roman', last_name: 'Makkarski', createdAt: new Date(), updatedAt: new Date() }
     ], {});
+    console.log('Students have been seeded.');
   },
 
   down: async (queryInterface, Sequelize) => {
+    console.log('Removing students...');
     await queryInterface.bulkDelete('Students', null, {});
+    console.log('Students have been removed.');
   }
 };
