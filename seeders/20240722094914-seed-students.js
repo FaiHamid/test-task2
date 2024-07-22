e0e2b6976a3db8f360809b1719d8566614ee8583
Faiza Hamid <hamid.faiza811@gmail.com>
2024-07-22 13:17:21 +0300

    added seeders4

--- seeders/20240722094914-seed-students.js
+++ seeders/20240722094914-seed-students.js
@@ -1,25 +1,20 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    console.log('Starting to seed students...');
+    await queryInterface.bulkInsert('\"Students\"', [
+      { first_name: 'Yulia', last_name: 'Prihodko', createdAt: new Date(), updatedAt: new Date() },
+      { first_name: 'Ivan', last_name: 'Zbaraski', createdAt: new Date(), updatedAt: new Date() },
+      { first_name: 'Faiza', last_name: 'Hamid', createdAt: new Date(), updatedAt: new Date() },
+      { first_name: 'Roman', last_name: 'Makkarski', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
+    console.log('Students have been seeded.');
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    console.log('Removing students...');
+    await queryInterface.bulkDelete('\"Students\"', null, {});
+    console.log('Students have been removed.');
   }
 };
